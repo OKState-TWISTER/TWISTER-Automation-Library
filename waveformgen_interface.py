96fc9f6192394e5ba2f159decd5b389a4fb0d8ed
twister control PC <>
2022-10-12 11:05:33 -0500

    add untested command and query functions

--- waveformgen_interface.py
+++ waveformgen_interface.py
@@ -13,16 +13,58 @@ class WaveformGenerator:
         self.debug = debug
         atexit.register(self.shutdown)
 
+        if self.debug:
+            print(f"Initializing Arbitrary Waveform Generator @ {visa_address}")
+
         rm = pyvisa.ResourceManager(visa_library)
         try:
-            self.awg = rm.open_resource(visa_address)
+            self.visa = rm.open_resource(visa_address)
         except pyvisa.errors.VisaIOError as e:
+            print(f"Make sure that the M8195A SFP is started:\n" +
+                   "Start Menu -> Keysight -> M8195 -> M8195 Soft Front Panel")
             print(f"Error connecting to device string '{visa_address}'. Is the device connected?")
             raise e
 
+        if self.debug:
+            idn_string = self.do_query("*IDN?").strip()
+            print(f"Connected to Arbitrary Waveform Generator: '{idn_string}'")
+
 
     def shutdown(self):
-        self.awg.close()
+        self.visa.close()
+
+
+
 
 
     # put useful functions to control AWG here
+
+
+
+
+
+    def do_command(self, command, hide_params=False):
+        if hide_params:
+            (header, data) = command.split(" ", 1)
+            if self.debug:
+                print(f"Cmd = '{header}'")
+        else:
+            if self.debug:
+                print(f"Cmd = '{command}'")
+
+        # inherent string casting? is this necessary?
+        self.visa.write("%s" % command)
+
+
+    def do_query(self, query):
+        if self.debug:
+            print(f"Qys = '{query}'")
+        result = self.visa.query("%s" % query)
+        return result
+
+
+    def do_query_ieee_block(self, query):
+        if self.debug:
+            print(f"Qyb = '{query}'")
+        result = self.visa.query_binary_values("%s" % query, datatype="s", container=bytes)
+        return result
