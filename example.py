953544f394543c5bb746107b04353f72b9242804
twister control PC <>
2022-10-16 15:39:57 -0500

    time get_waveform for comparison

--- example.py
+++ example.py
@@ -1,3 +1,5 @@
+from time import perf_counter as time
+
 import matplotlib.pyplot as plt
 
 from oscilloscope_interface import Oscilloscope
@@ -9,16 +11,20 @@ scope.set_trigger_source(3)
 #scope.do_command(':ACQ:COMP 60')
 #scope.do_command(':ACQ:POIN 64')
 
+start = time()
+values = scope.get_waveform_words(channels=[2,3,1,4])
+end = time()
+print(f"Took {end - start} seconds")
 
-values = scope.get_waveform_words(channels=[2,3])
 
 print(f"len {len(values)}")
 
-
 figure, axis = plt.subplots(2, 1)
+
+axis[0].plot(values[0])
+axis[0].set_title("Channel 2")
   
-axis[0, 0].plot(values[0])
-axis[0, 0].set_title("Channel 2")
-  
-axis[1, 0].plot(values[1])
-axis[1, 0].set_title("Channel 3")
+axis[1].plot(values[1])
+axis[1].set_title("Channel 3")
+
+plt.show()
