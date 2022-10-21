8fadddc1dd7bb2c81690ec72c57ec1603886beea
twister control PC <>
2022-10-21 16:09:55 -0500

    add freq and phase adjust functions for psg

--- signalgen_interface.py
+++ signalgen_interface.py
@@ -18,21 +18,51 @@ class SignalGenerator:
                 # These are the VISA addresses of the two signal generators we have at time of writing
                 if device_no == 1:
                     visa_address = "TCPIP0::10.10.10.21::inst0::INSTR"
+                    self.name = "psg1"
                 elif device_no == 2:
                     visa_address = "TCPIP0::10.10.10.22::inst0::INSTR"
+                    self.name = "psg2"
                 else:
                     raise ValueError("Valid device_no: 1, 2")
 
         rm = pyvisa.ResourceManager(visa_library)
         try:
-            self.psg = rm.open_resource(visa_address)
+            pyvisa.resources.Resource: self.visa  # type hinting
+            self.visa = rm.open_resource(visa_address)
         except pyvisa.errors.VisaIOError as e:
             print(f"Error connecting to device string '{visa_address}'. Is the device connected?")
             raise e
 
 
     def shutdown(self):
-        self.psg.close()
+        self.visa.close()
 
 
-    # put useful functions to control ASGs here
+    
+    def set_frequency(self, frequency: float):
+        self.do_command(f":FREQuency:FIXed {frequency:.2E}")
+        if self.debug:
+            print(f"Set {self.name} frequency to {self.do_query(':FREQuency:FIXed?')}")
+
+
+    def set_phase(self, degree: float):
+        self.do_command(f":PHASe {degree}DEG")
+        if self.debug:
+            print(f"Set {self.name} phase to {self.do_query(':PHASe?')} radians")
+
+
+    def set_phase_reference(self):
+        self.do_command(":PHASe:REFerence")
+        if self.debug:
+            print(f"Set {self.name} phase reference")
+
+
+
+    def do_command(self, command):
+        """Executes SCPI command on the PSG."""
+        self.visa.write(str(command))
+
+
+    def do_query(self, query):
+        result = self.visa.query(str(query))
+        return result.rstrip()
