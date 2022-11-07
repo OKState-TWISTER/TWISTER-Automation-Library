ff8ba959430708df0dc612493322a071833ec094
twister control PC <>
2022-11-07 14:49:59 -0600

    fix endianness of waveform files

--- example.py
+++ example.py
@@ -8,20 +8,20 @@ from signalgen_interface import SignalGenerator
 import twister_utils
 
 scope = Oscilloscope(debug=True)
-#awg = WaveformGenerator(debug=True)
+awg = WaveformGenerator(debug=True)
 psg1 = SignalGenerator(device_no=1, debug=True)
 psg2 = SignalGenerator(device_no=2, debug=True)
 
 #scope.set_trigger_source(3)
 #scope.view_one_segment()
 
-filepath = r"C:\Users\UTOL\Desktop\MRI-Testbed-main\M4N4_10.00Gbd_54.0Gsps_0.90Beta.bin"
-samplerate = 55e9
+filepath = r"C:\Users\UTOL\Desktop\12.5GHz_sine_6.3950Gsps.bin"
+samplerate = 63.95e9
 
-#with awg.enable_output():
-while True:
+with awg.enable_output():
     start = time()
-    twister_utils.peak_phase(debug=False)
+    awg.load_waveform(filepath, samplerate)
     end = time()
-    print(f"took {end-start} seconds")
+    print(f"time: {end-start}")
     input()
+
